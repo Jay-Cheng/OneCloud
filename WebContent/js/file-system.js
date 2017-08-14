fde0c281743e3898950a214c2d25930e6d40fddf
jasamwdw <924760408@qq.com>
2017-08-14 15:08:47 +0800

    实现了文件系统

--- WebContent/js/file-system.js
+++ WebContent/js/file-system.js
@@ -75,45 +75,47 @@ var file5 = {
 var files = [dir1, file1, file2, file3, dir2, dir3, dir4, file4, file5];
 
 $(function() {
+    $("#disk_file_path li").click(goBack);
     showDirContents("home");
-    $(".disk-file-item").click(function() {
-        if ($(this).attr("data-enter") != undefined) {
-            var node = $('<li></li>');
-            var dirName = $(this).find(".file-name").text();
-            node.append('<a href="javascript:void(0)">' + dirName + '</a>');
-            node.attr("data-enter", $(this).attr("data-enter"));
-            node.appendTo($("#disk_file_path"));
-            showDirContents($(this).attr("data-enter"));
-        }
-    });
 });
 /*
  * 通用函数：动态生成某个文件夹下的所有文件
  * dir是文件夹的路径   
  */
 function showDirContents(dir) {
+    /* 隐藏当前文件夹内容 */
     $("#all ul").hide();
-    alert(dir);
+    /* 如果需展示的文件夹之前已经生成过，则显示对应节点，返回 */
     var node = $('[data-dir="' + dir + '"]');
-    alert("here");
     if (node.length != 0) {
         node.show();
         return;
     }
+    /* 如果需展示的文件夹不存在 */
+    /* 生成一个文件夹节点<ul>，设置data-dir属性，该属性是该文件夹节点的路径*/
     node = $("<ul></ul>");
     node.attr("data-dir", dir);
     for (var i = 0;i < files.length; i++) {
         if (files[i].localURL == dir) {
+            /* 如果文件存在于该文件夹下 */
             /* 生成一个该文件的<li>节点 */
             var fileImg = "img/icon/";
             var fileName = files[i].localName + "." + files[i].localType;
-            var fileSize = getReadableSize(files[i].size);
+            var fileSize = files[i].size;
+            if (fileSize != null) {
+                fileSize = getReadableSize(files[i].size);
+            }
             var lastModified = files[i].lastModified;
             switch(files[i].localType) {
-                case "dir": 
-                fileName = files[i].localName;
-                fileSize = "";
-                fileImg += "folder";
+                case "dir":
+                if (fileSize == null) {
+                    fileName = files[i].localName;
+                    fileSize = "";
+                    fileImg += "folder";                    
+                } else {
+                    // 文件后缀为dir的情况
+                    fileImg += "file";
+                }
                 break;
                 case "mp4": 
                 fileImg += "video";
@@ -128,12 +130,45 @@ function showDirContents(dir) {
             var fileNode = $('<li class="disk-file-item"></li>');
             fileNode.append('<div class="file-head"><div class="select"><input type="checkbox"></div><div class="thumb"><img src="' + fileImg + '" class="thumb-icon"></div><div class="file-title"><span class="file-name">' + fileName + '</span></div></div>');
             fileNode.append('<div class="file-info"><span class="file-size">' + fileSize + '</span><span class="file-time">' + lastModified + '</span></div>');
-            /* 如果该节点是文件夹，设置其data-enter属性为该节点的路径 */
+            /* 如果该节点是文件夹，设置其data-enter属性为该节点的路径，并绑定相应事件 */
             if (files[i].localType == "dir" && files[i].size == null) {
                 fileNode.attr("data-enter", dir + "/" + files[i].localName);
+                fileNode.on("click", enterDir);
             }
             fileNode.appendTo(node);
         }
     }
     node.appendTo($("#all"));
+}
+
+/* 
+ * 单击文件夹后，更新面包屑导航并列出该文件夹下的所有文件
+ * 该函数绑定到文件夹<li>节点的click事件上
+ */
+function enterDir() {
+    /* 判断该文件是否是文件夹 */
+    if ($(this).attr("data-enter") != undefined) {
+        /* 生成一个该文件夹的面包屑导航节点 */
+        var node = $('<li></li>');
+        var dirName = $(this).find(".file-name").text();
+        node.append('<a href="javascript:void(0)">' + dirName + '</a>');
+        node.attr("data-enter", $(this).attr("data-enter"));
+        node.on("click", goBack);
+        $("#disk_file_path li").children().removeClass("active");
+        node.children().addClass("active");
+        node.appendTo($("#disk_file_path"));
+        /* 列出该文件夹下的所有文件 */ 
+        showDirContents($(this).attr("data-enter"));
+    }    
+}
+
+/*
+ * 面包屑导航click时绑定的事件处理函数
+ */
+function goBack() {
+    /* 把本节点之后的sibling全部移除 */
+    $(this).nextAll().remove();
+    $("#disk_file_path li").children().removeClass("active");
+    $(this).children().addClass("active");
+    showDirContents($(this).attr("data-enter"));
 }
