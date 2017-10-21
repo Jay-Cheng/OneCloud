b287b071e48ac35deccb0ba778a4a7ed12950cb7
jay <924760408@qq.com>
2017-10-21 14:44:09 +0800

    消除测试类与junit的命名冲突

---
+++ src/com/zhengzijie/onecloud/manager/util/ProjectTest.java
@@ -0,0 +1,8 @@
+package com.zhengzijie.onecloud.manager.util;
+
+public class ProjectTest {
+    
+    public void someTest() {
+        
+    }
+}
--- src/com/zhengzijie/onecloud/manager/util/Test.java
+++
@@ -1,8 +0,0 @@
-package com.zhengzijie.onecloud.manager.util;
-
-public class Test {
-
-    public static void main(String[] args) {
-    }
-
-}
